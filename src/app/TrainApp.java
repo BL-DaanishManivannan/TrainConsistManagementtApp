834721c5e6fb958012314bb9281bccf9c5d2546c
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-06-18 16:40:20 +0530

    UC1: Initialize train consist

--- src/app/TrainApp.java
+++ src/app/TrainApp.java
@@ -1,7 +1,13 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Train Consist Management App");
+        
+        List<String> train = new ArrayList<>();
+        System.out.println("Initial bogie count: " + train.size());
     }
 }
